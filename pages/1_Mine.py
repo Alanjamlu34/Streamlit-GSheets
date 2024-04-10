128b23b289ef869dc55d4d348c03045ff5f57db6
Alanjamlu34 <alanjamlu09@gmail.com>
2024-04-11 04:19:14 +0700

    Last update (maybe)

--- pages/1_Mine.py
+++ pages/1_Mine.py
@@ -13,4 +13,9 @@ st.markdown("<a href='https://youtu.be/bLvqoHBptjg?si=Ac2x8T3Ja8MSbecl'>FORRSET 
 st.markdown("<a href='https://youtu.be/PsD0NpFSADM?si=TsQE1K7PGEHFlL4m'>500 DAYS OF SUMMER</a>.</font>",unsafe_allow_html=True)
 
 st.title("Playlist")
-st.markdown("<a href='https://open.spotify.com/playlist/3HwtZRMR0HqP5Hcb55pB4f?si=539b8ffbbaa64251'>STOP THIS TRAIN</a>.</font>",unsafe_allow_html=True)
+st.image('Data/lAGU.png')
+st.markdown("Playlist <a href='https://open.spotify.com/playlist/3HwtZRMR0HqP5Hcb55pB4f?si=539b8ffbbaa64251'>STOP THIS TRAIN</a>.</font>",unsafe_allow_html=True)
+
+st.title("SEE ALL")
+st.markdown("WEB <a href='https://tisian1.odoo.com/playlist'>TisIan</a>.</font>",unsafe_allow_html=True)
+st.markdown("Film <a href='https://letterboxd.com/TisIan/'>Letterboxd</a>.</font>",unsafe_allow_html=True)
